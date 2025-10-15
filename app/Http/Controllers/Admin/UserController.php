0bca530fc73b6f5d21f266b1f257c6cf57c8fd59
pvkhanh <pvkhanh.tech@gmail.com>
2025-10-15 23:51:06 +0700

    15/10/2025 - Hiển thị và giao diện User

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -9,19 +9,14 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
 
-    // Hiển thị danh sách user
     public function index(Request $request)
     {
         $query = User::query();
-
-        // Search theo username hoặc email nếu có input
         if ($request->filled('search')) {
             $search = $request->search;
             $query->where('username', 'like', "%{$search}%")
                 ->orWhere('email', 'like', "%{$search}%");
         }
-
-        // Pagination, 10 user/trang
         $users = $query->orderBy('id', 'desc')->paginate(5);
 
         return view('admin.users.index', compact('users'));
